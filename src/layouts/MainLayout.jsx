7fdf9f93d34b97a4ed01441ac8daffc8a9b16afb
plabondas001 <plabondas001@gmail.com>
2026-05-01 21:39:51 +0600

    fix: import path

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,6 @@ import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Brands from '../Components/brands/Brands';
 
 const MainLayout = () => {
     return (
