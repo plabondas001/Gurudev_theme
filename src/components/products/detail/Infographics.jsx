53c939230f2c310573dceebc78beda68590091a2
plabondas001 <plabondas001@gmail.com>
2026-05-11 21:13:05 +0600

    fix: Infographic section

--- src/components/products/detail/Infographics.jsx
+++ src/components/products/detail/Infographics.jsx
@@ -7,38 +7,38 @@ import { GiProcessor } from "react-icons/gi";
 
 const Infographics = () => {
   return (
-    <div className="bg-[#e5f0e8] rounded-2xl p-5 gap-5 sm:p-4 sm:gap-4 lg:grid lg:grid-cols-5 text-center mb-5">
-      <div className="flex gap-3 lg:gap-1 lg:flex-col items-center justify-start py-2 lg:py-0">
+    <div className="bg-[#e5f0e8] rounded-2xl p-5 gap-5 sm:p-4 sm:gap-4 mb-5 md:space-y-3">
+      <div className="flex gap-3 items-center justify-start py-2 lg:py-0">
         <IoCameraOutline className="text-primary" size={25} />
-        <h1 className="font-semibold text-sm w-fit lg:w-auto">
+        <h1 className="font-semibold text-sm md:text-lg w-fit lg:w-auto">
           50 Mp Triple Camera
         </h1>
       </div>
 
-      <div className="flex gap-3 lg:gap-1 lg:flex-col items-center justify-start py-2 lg:py-0">
+      <div className="flex gap-3 items-center justify-start py-2 lg:py-0">
         <HiOutlineDevicePhoneMobile className="text-primary" size={25} />
-        <h1 className="font-semibold text-sm w-fit lg:w-auto">
+        <h1 className="font-semibold text-sm md:text-lg w-fit lg:w-auto">
           6.67" AMOLED Display
         </h1>
       </div>
 
-      <div className="flex gap-3 lg:gap-1 lg:flex-col items-center justify-start py-2 lg:py-0">
+      <div className="flex gap-3 items-center justify-start py-2 lg:py-0">
         <MdBatteryCharging80 className="text-primary" size={25} />
-        <h1 className="font-semibold text-sm w-fit lg:w-auto">
+        <h1 className="font-semibold text-sm md:text-lg w-fit lg:w-auto">
           5000mAh Battery
         </h1>
       </div>
 
-      <div className="flex gap-3 lg:gap-1 lg:flex-col items-center justify-start py-2 lg:py-0">
+      <div className="flex gap-3 items-center justify-start py-2 lg:py-0">
         <Zap size={25} className="text-primary" />
-        <h1 className="font-semibold text-sm w-fit lg:w-auto">
+        <h1 className="font-semibold text-sm md:text-lg w-fit lg:w-auto">
           50 Mp Triple Camera
         </h1>
       </div>
 
-      <div className="flex gap-3 lg:gap-1 lg:flex-col items-center justify-start py-2 lg:py-0">
+      <div className="flex gap-3 items-center justify-start py-2 lg:py-0">
         <GiProcessor size={25} className="text-primary" />
-        <h1 className="font-semibold text-sm w-fit lg:w-auto">
+        <h1 className="font-semibold text-sm md:text-lg w-fit lg:w-auto">
           Mediatek Helio G92 Max
         </h1>
       </div>
